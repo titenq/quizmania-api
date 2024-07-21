da6da789132e8aeeb9ab717e4130f76e2ce0b337
titenq <titenq@gmail.com>
2024-07-21 19:42:48 -0300

    delete routes copy

--- src/routes copy/pingRoute.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
-
-const pingRoute = async (fastify: FastifyInstance, options: any) => {
-  fastify.get('/ping', async (request: FastifyRequest, reply: FastifyReply) => {
-    return reply.status(200).send({ ping: 'pong' });
-  });
-}
-
-export default pingRoute;
